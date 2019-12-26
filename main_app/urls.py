afada5babb9f0366d9fac8af8862366a3d1e57a0
linnaek <linnaekraemer@gmail.com>
2019-12-26 15:08:52 -0600

    add style details

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -11,9 +11,12 @@ urlpatterns = [
     path('teas/<int:pk>/delete/', views.TeaDelete.as_view(), name='teas_delete'),
     path('teas/<int:tea>/add_drinking/', views.add_drinking, name='add_drinking'),
     path('teas/<int:tea_id>/assoc_store/<int:store_id>/', views.assoc_store, name="assoc_store"),
+    path('teas/<int:tea_id>/unassoc_store/<int:store_id>/', views.unassoc_store, name="unassoc_store"),
     path('stores/', views.StoreList.as_view(), name='stores_list'),
     path('stores/create/', views.StoreCreate.as_view(), name='stores_create'),
     path('stores/<int:pk>/update/', views.StoreUpdate.as_view(), name='stores_update'),
     path('stores/<int:pk>/', views.StoreDetailView.as_view(), name='store_detail'),
-    path('stores/<int:pk>/delete/', views.StoreDelete.as_view(), name='stores_delete')
+    path('stores/<int:pk>/delete/', views.StoreDelete.as_view(), name='stores_delete'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.signup, name='signup')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import DrinkingForm
 from .models import Tea, Store, Drinking 
 
@@ -12,10 +16,12 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def teas_index(request):
-    teas = Tea.objects.all()
+    teas = Tea.objects.filter(user=request.user)
     return render(request, 'teas/index.html', {'teas': teas})
 
+@login_required
 def teas_detail(request, tea_id):
     tea = Tea.objects.get(id=tea_id)
     stores_tea_doesnt_have = Store.objects.exclude(id__in = tea.stores.all().values_list('id'))
@@ -26,24 +32,26 @@ def teas_detail(request, tea_id):
         'stores': stores_tea_doesnt_have
          })
 
-class TeaCreate(CreateView):
+class TeaCreate(LoginRequiredMixin, CreateView):
+    model = Tea
+    fields = ['name', 'category', 'water_temp', 'steeping_time', 'notes']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class TeaUpdate(LoginRequiredMixin, UpdateView):
     model = Tea
     fields = ['name', 'category', 'water_temp', 'steeping_time', 'notes']
 
     def form_valid(self, form):
         return super().form_valid(form)
 
-class TeaUpdate(UpdateView):
-    model = Tea
-    fields = ['name', 'category', 'water_temp', 'steeping_time', 'notes']
-
-    def form_valid(self, form):
-        return super().form_valid(form)
-
-class TeaDelete(DeleteView):
+class TeaDelete(LoginRequiredMixin, DeleteView):
     model = Tea
     success_url = '/teas/'
 
+@login_required
 def add_drinking(request, tea):
     form = DrinkingForm(request.POST)
     print(form)
@@ -53,27 +61,45 @@ def add_drinking(request, tea):
         new_drinking.save()
     return redirect('detail', tea_id=tea)
 
+@login_required
 def assoc_store(request, tea_id, store_id):
     Tea.objects.get(id=tea_id).stores.add(store_id)
     return redirect('detail', tea_id=tea_id)
 
-class StoreList(ListView):
+@login_required
+def unassoc_store(request, tea_id, store_id):
+    Tea.objects.get(id=tea_id).stores.remove(store_id)
+    return redirect('detail', tea_id=tea_id)
+
+class StoreList(LoginRequiredMixin, ListView):
     model = Store
 
-class StoreCreate(CreateView):
+class StoreCreate(LoginRequiredMixin, CreateView):
     model = Store
     fields = ['name', 'website', 'address', 'city', 'phone']
 
-class StoreUpdate(UpdateView):
+class StoreUpdate(LoginRequiredMixin, UpdateView):
     model = Store
     fields = ['name', 'website', 'address', 'city', 'phone']
 
-class StoreDetailView(DetailView):
+class StoreDetailView(LoginRequiredMixin, DetailView):
     model = Store
     
-class StoreDelete(DeleteView):
+class StoreDelete(LoginRequiredMixin, DeleteView):
     model = Store
     success_url = '/stores/'
 
-
-
+@login_required
+def signup(request):
+    error_message=''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
